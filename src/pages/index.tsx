56d5d5d6575c603cb7bb93f6c7de253f4e81075c
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-02-09 16:15:10 -0800

    Added Comment Creation
    strict mode is lifted due to double-rendered comments

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,35 +7,49 @@ import Modal from 'react-modal';
 type Post = {
   title: string
   content: string
+  comments: Comment[]
+}
+
+type Comment = {
+  content: string,
 }
 
 const examplePost: Post = {
   title: "GitHub lays off 10% and goes fully remote",
-  content: "Source: https://techcrunch.com/2023/02/09/github-lays-off-10-and-goes-fully-remote/ The tech layoffs keep on coming. Microsoft-owned GitHub today announced that it is laying off 10% of its staff through the end of the company’s fiscal year. Before this announcement, which was first reported by Fortune, GitHub had about 3,000 employees. The company will also shutter all of its offices as their leases end, in part because of their low utilization, and move to a remote-first culture. GitHub will also continue its hiring freeze, which it first announced in January, and also make a number of other internal changes to “protect the short-term health” of its business. “We announced a number of difficult but necessary decisions and budgetary realignments to both protect the health of our business in the short term and grant us the capacity to invest in our long-term strategy moving forward. You can view our CEO’s full message to employees with additional details on these changes below,” a company spokesperson told us. In a move that’s a bit unorthodox for a company that has prided itself on remaining independent of its corporate owner, GitHub is also moving to Teams for its video conferencing needs. And as another sign of cost cutting, it’s moving its laptop refresh cycle from three to four years. “Although our entire leadership team has carefully deliberated this step and come to agreement, ultimately, as CEO the decision is mine. I recognize this will be difficult on you all, and we will approach this period with the utmost respect for every Hubber,” GitHub CEO Thomas Dohmke wrote in an email to the company’s staff today. He notes that he wants the company to become the “developer-first engineering system for the world of tomorrow,” with a strong focus on AI. Given GitHub’s recent focus on its Copilot and the overall shift at Microsoft toward AI-everything, that’s maybe not unexpected."
+  content: "Source: https://techcrunch.com/2023/02/09/github-lays-off-10-and-goes-fully-remote/ The tech layoffs keep on coming. Microsoft-owned GitHub today announced that it is laying off 10% of its staff through the end of the company’s fiscal year. Before this announcement, which was first reported by Fortune, GitHub had about 3,000 employees. The company will also shutter all of its offices as their leases end, in part because of their low utilization, and move to a remote-first culture. GitHub will also continue its hiring freeze, which it first announced in January, and also make a number of other internal changes to “protect the short-term health” of its business. “We announced a number of difficult but necessary decisions and budgetary realignments to both protect the health of our business in the short term and grant us the capacity to invest in our long-term strategy moving forward. You can view our CEO’s full message to employees with additional details on these changes below,” a company spokesperson told us. In a move that’s a bit unorthodox for a company that has prided itself on remaining independent of its corporate owner, GitHub is also moving to Teams for its video conferencing needs. And as another sign of cost cutting, it’s moving its laptop refresh cycle from three to four years. “Although our entire leadership team has carefully deliberated this step and come to agreement, ultimately, as CEO the decision is mine. I recognize this will be difficult on you all, and we will approach this period with the utmost respect for every Hubber,” GitHub CEO Thomas Dohmke wrote in an email to the company’s staff today. He notes that he wants the company to become the “developer-first engineering system for the world of tomorrow,” with a strong focus on AI. Given GitHub’s recent focus on its Copilot and the overall shift at Microsoft toward AI-everything, that’s maybe not unexpected.",
+  comments: [
+    {
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit deserunt eos minus. Adipisci dolor aperiam exercitationem corporis reprehenderit vitae repellat blanditiis quibusdam recusandae numquam. Perferendis nam totam excepturi veritatis modi!"
+    }
+  ]
 }
 
 const Home: NextPage = () => {
   const [feed, setFeed] = useState<Post[]>([examplePost])
   const [createPostModalVisible, setCreatePostModalVisible] = useState(false);
   const [editPostModalVisible, setEditPostModalVisible] = useState(false);
-  const [createPostFormState, setCreatePostFormState] = useState({
+  const [createPostFormState, setCreatePostFormState] = useState<Post>({
     title: "",
     content: "",
+    comments: [],
   });
-  const [editPostFormState, setEditPostFormState] = useState({
+  const [editPostFormState, setEditPostFormState] = useState<Post & { id: number }>({
     id: -1,
     title: "",
     content: "",
+    comments: []
   });
+  const [addCommentStates, setAddCommentStates] = useState<string[]>([""])
 
   const handleCreateBlogFormSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     setFeed((feed) => [createPostFormState, ...feed]);
+    setAddCommentStates((state) => ["", ...state]);
 
     setCreatePostModalVisible(false);
 
-    setCreatePostFormState({ title: "", content: "" });
+    setCreatePostFormState({ title: "", content: "", comments: [] });
   };
 
   const handleEditBlogFormSubmit = (event: FormEvent<HTMLFormElement>) => {
@@ -48,7 +62,31 @@ const Home: NextPage = () => {
 
     setEditPostModalVisible(false);
 
-    setEditPostFormState({ id: -1, title: "", content: "" });
+    setEditPostFormState({ id: -1, title: "", content: "", comments: [] });
+  };
+
+  const handleCreateCommentSubmit = (event: FormEvent<HTMLFormElement>, id: number) => {
+    event.preventDefault();
+
+    const commentContent = addCommentStates[id]!;
+
+    setAddCommentStates((states) => {
+      states[id] = "";
+
+      return [...states]
+    });
+
+    setFeed((feed) => {
+      const updatedPost = {
+        ...feed[id]!, comments: [...feed[id]!.comments, {
+          content: commentContent
+        }]
+      }
+
+      feed.splice(id, 1, updatedPost)
+
+      return [...feed];
+    })
   };
 
   return (
@@ -60,10 +98,11 @@ const Home: NextPage = () => {
       </Head>
       <button onClick={() => setCreatePostModalVisible(true)}>Create a Post</button>
       <Modal
+        ariaHideApp={false}
         isOpen={createPostModalVisible}
         onRequestClose={() => {
           setCreatePostModalVisible(false);
-          setCreatePostFormState({ title: "", content: "" });
+          setCreatePostFormState({ title: "", content: "", comments: [] });
         }}
         className="bg-cyan-500 absolute w-4/5 h-4/5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
       >
@@ -71,7 +110,7 @@ const Home: NextPage = () => {
           className="bg-gray-300 absolute right-2 top-2 w-12 h-12 rounded-full font-bold"
           onClick={() => {
             setCreatePostModalVisible(false);
-            setCreatePostFormState({ title: "", content: "" });
+            setCreatePostFormState({ title: "", content: "", comments: [] });
           }}
         >X</button>
         <div className="p-12 bg-gray-400 w-5/6 h-5/6 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -98,6 +137,7 @@ const Home: NextPage = () => {
       </Modal>
 
       <Modal
+        ariaHideApp={false}
         isOpen={editPostModalVisible}
         shouldCloseOnOverlayClick={false}
         shouldCloseOnEsc={false}
@@ -107,7 +147,7 @@ const Home: NextPage = () => {
           className="bg-gray-300 absolute right-2 top-2 w-12 h-12 rounded-full font-bold"
           onClick={() => {
             setEditPostModalVisible(false);
-            setEditPostFormState({ id: -1, title: "", content: "" });
+            setEditPostFormState({ id: -1, title: "", content: "", comments: [] });
           }}
         >X</button>
         <div className="p-12 bg-gray-400 w-5/6 h-5/6 relative top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
@@ -135,7 +175,7 @@ const Home: NextPage = () => {
 
       <div className="flex flex-col items-center w-full">
         {feed.map((post, id) =>
-          <div className="border border-solid border-black w-5/6 my-2 relative">
+          <div className="border border-solid border-black w-5/6 my-2 relative" key={id}>
             <div className="flex flex-row justify-between">
               <div className="text-3xl">{post.title}</div>
               <div className="w-12 h-12">
@@ -144,7 +184,7 @@ const Home: NextPage = () => {
                   menuClassName="bg-white border-solid border-black border cursor-pointer z-10"
                 >
                   <MenuItem onClick={() => {
-                    setEditPostFormState({ id, title: post.title, content: post.content })
+                    setEditPostFormState({ id, title: post.title, content: post.content, comments: post.comments })
                     setEditPostModalVisible(true)
                   }}>
                     Edit
@@ -154,6 +194,12 @@ const Home: NextPage = () => {
                       feed.splice(id, 1)
 
                       return [...feed];
+                    });
+
+                    setAddCommentStates((state) => {
+                      state.splice(id, 1);
+
+                      return [...state];
                     })
                   }}>
                     Delete
@@ -162,6 +208,30 @@ const Home: NextPage = () => {
               </div>
             </div>
             <p className="text-base break-words">{post.content}</p>
+            <div className="border-t border-black border-solid flex flex-col">
+              <div className="text-2xl">Comments</div>
+              {post.comments.map((comment, commentId) =>
+                <div className="w-full flex flex-row items-start my-2" key={commentId}>
+                  <img src="https://i.pravatar.cc/300" className="w-12 rounded-full" />
+                  <p className="text-base break-words w-11/12">{comment.content}</p>
+                </div>
+              )}
+              <div className="w-full flex flex-row items-center my-2">
+                <img src="https://i.pravatar.cc/300" className="w-12 rounded-full" />
+                <form onSubmit={(event) => handleCreateCommentSubmit(event, id)}>
+                  <textarea
+                    value={addCommentStates[id]}
+                    onChange={(event) => setAddCommentStates((states) => {
+
+                      states[id] = event.target.value;
+
+                      return [...states];
+                    })}
+                  />
+                  <input type="submit" value="Submit" />
+                </form>
+              </div>
+            </div>
           </div>
         )}
       </div>
