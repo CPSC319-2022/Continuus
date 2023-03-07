d2692bac5941041fa2f1218b80fb333243423fef
itsarune <42703774+itsarune@users.noreply.github.com>
2023-03-06 20:04:22 -0800

    wip at class

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,19 @@
 terraform {
-  cloud {
-    organization = "continuus"
-
-    workspaces {
-      name = "continuus"
+  backend "gcs" {
+    bucket  = "artifacts.automatic-bot-376307.appspot.com"
+    prefix  = "state"
+  }  
+  required_version = ">= 0.13"  
+  required_providers { 
+    google = {
+      source = "hashicorp/google"
+      version = "4.56.0"
     }
   }
+}
+provider "google" {
+  project = "<gcp_project_id>"
+  region  = "<regione_name>"
+  zone    = "<zone_name>"
 }
 
