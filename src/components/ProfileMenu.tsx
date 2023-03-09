cc8cd7a159961918491616830a2e4839fe52b9e5
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-03-09 08:02:32 -0800

    re-abstraction

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -6,27 +6,17 @@ import btn_google from '../../public/btn_google.png'
 
 export const ProfileMenu: React.FC = () => {
     const { status } = useSession();
-
-    const _renderProfileImage = () => (
-        <div
-            className="rounded-full aspect-square w-12 border border-solid border-gray-400"
-            style={{ backgroundImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb1KzK9eXLao5CnsRObDT6gDyGG-u2HHyYoVBgvBnW&s)" }}
-        />
-    );
-
-    const _renderMenuButton: React.FC<{ label: ReactNode, onClick?: () => void }> = ({ label, onClick }) => (
-        <MenuItem
-            onClick={onClick}
-            className="w-full px-4 py-2 cursor-pointer text-center hover:bg-gray-200"
-        >
-            {label}
-        </MenuItem>
-    );
+    const menuButtonClassName = "w-full px-4 py-2 cursor-pointer text-center hover:bg-gray-200";
 
     return (
         <Menu
             menuButton={
-                <MenuButton>{_renderProfileImage()}</MenuButton>
+                <MenuButton>
+                    <div
+                        className="rounded-full aspect-square w-12 border border-solid border-gray-400"
+                        style={{ backgroundImage: "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQb1KzK9eXLao5CnsRObDT6gDyGG-u2HHyYoVBgvBnW&s)" }}
+                    />
+                </MenuButton>
             }
             align="end"
             aria-label="Profile"
@@ -38,21 +28,27 @@ export const ProfileMenu: React.FC = () => {
                     switch (status) {
                         case "authenticated":
                             return [
-                                _renderMenuButton({ label: "Profile" }),
-                                _renderMenuButton({ label: "Sign Out" })
+                                <MenuButton className={menuButtonClassName} key="profile">
+                                    Profile
+                                </MenuButton>,
+                                <MenuButton className={menuButtonClassName} key="sign-out">
+                                    Sign-Out
+                                </MenuButton>
                             ];
                         case "loading":
-                            return _renderMenuButton({ label: "Loading..." });
+                            return <MenuButton className={menuButtonClassName}>Loading...</MenuButton>;
                         case "unauthenticated":
-                            return _renderMenuButton({
-                                label: (
+                            return (
+                                <MenuButton
+                                    className={menuButtonClassName}
+                                    onClick={() => void signIn("google")}
+                                >
                                     <div className="flex justify-center items-center relative">
                                         <Image src={btn_google} alt="Sign-In/Up With Google" className="mr-2" />
                                         <span>Sign-In/Up</span>
                                     </div>
-                                ),
-                                onClick: () => void signIn("google")
-                            });
+                                </MenuButton>
+                            );
                     }
                 })()
             }
