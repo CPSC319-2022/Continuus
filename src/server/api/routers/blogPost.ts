f4378ec23ded0c9fad9da7ddfb66dfba96cf23c6
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-03-09 10:50:09 -0800

    Adding BlogPost entity and createBlogPost procedure

---
+++ src/server/api/routers/blogPost.ts
@@ -0,0 +1,21 @@
+import { BlogPostCreateOneSchema } from "../../../../prisma/generated/schemas";
+
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const blogPostRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(BlogPostCreateOneSchema)
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUniqueOrThrow({
+        where: {
+          id: ctx.session.user.id
+        }
+      });
+
+      if (!["CONTRIBUTOR", "ADMIN"].includes(user.role)) {
+        throw new Error("UNAUTHORIZED");
+      }
+
+      return await ctx.prisma.blogPost.create(input);
+    }),
+});
