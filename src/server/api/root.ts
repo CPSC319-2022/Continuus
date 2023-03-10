6cfffb3b964fefbd18666c021eae7c76d67b2d47
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-03-10 00:22:54 -0800

    Added user router and currentUser query

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,6 +1,7 @@
 import { createTRPCRouter } from "./trpc";
 import { exampleRouter } from "./routers/example";
 import { blogPostRouter } from "./routers/blogPost";
+import { userRouter } from "./routers/user";
 
 /**
  * This is the primary router for your server.
@@ -10,6 +11,7 @@ import { blogPostRouter } from "./routers/blogPost";
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
   blogPost: blogPostRouter,
+  user: userRouter,
 });
 
 // export type definition of API
