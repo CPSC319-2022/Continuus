806cfb9769820b2023b5702b630510f920f38cb9
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2023-03-10 09:45:40 -0800

    Con 81 blog post persistence (#25)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,22 @@
-import { defineConfig } from "vitest/config";
+import { configDefaults, defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import { loadEnvConfig } from "@next/env";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom",
-  },
+export default defineConfig(() => {
+  process.env['DATABASE_URL'] = 'postgresql://postgres:password@localhost:5432/continuus'
+  process.env['NEXTAUTH_URL'] = 'http://localhost:3000'
+  process.env['GOOGLE_CLIENT_ID'] = 'TEST_PLACEHOLDER'
+  process.env['GOOGLE_CLIENT_SECRET'] = 'TEST_PLACEHOLDER'
+  loadEnvConfig(process.cwd());
+
+  return {
+    plugins: [react()],
+    test: {
+      environment: "jsdom",
+      coverage: {
+        exclude: [...configDefaults.coverage.exclude!, "**/generated/**", "src/env"]
+      },
+    },
+  }
 });
