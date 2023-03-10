33cf77b26a28c89f6af0fa2de6e0974a5ad0b0d3
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-03-09 19:34:18 -0800

    Making environment variables make sense

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import { loadEnvConfig } from "@next/env";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => {
+  process.env['DATABASE_URL'] = 'postgresql://postgres:password@localhost:5432/continuus'
+  process.env['NEXTAUTH_URL'] = 'http://localhost:3000'
+  process.env['GOOGLE_CLIENT_ID'] = 'TEST_PLACEHOLDER'
+  process.env['GOOGLE_CLIENT_SECRET'] = 'TEST_PLACEHOLDER'
   loadEnvConfig(process.cwd());
 
   return {
