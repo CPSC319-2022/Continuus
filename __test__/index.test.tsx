21a1415582b8359917ccfe9ec7ce44b791c04e15
Altay Batuhan <45828884+altarrok@users.noreply.github.com>
2023-03-13 15:13:35 -0700

    Con 5 sign in Deploying to prod (#28)
    Co-authored-by: Scott <46171819+scott-m-king@users.noreply.github.com>
    Co-authored-by: Arun <arun.sfc@outlook.com>
    Co-authored-by: itsarune <42703774+itsarune@users.noreply.github.com>

---
+++ __test__/index.test.tsx
@@ -0,0 +1,45 @@
+import { expect, describe, it, vi, beforeEach } from "vitest";
+import { render, screen, within } from "@testing-library/react";
+import Home from "../src/pages/index";
+
+vi.mock("next-auth/react", () => ({
+  useSession: () => ({
+    data: {
+      expires: "1",
+      user: { id: "1", email: "a", name: "hi", image: "c" },
+    },
+  }),
+}));
+
+vi.mock("../src/utils/api", () => ({
+  api: {
+    example: {
+      hello: {
+        useQuery: () => ({
+          data: {
+            greeting: "Hello world",
+          },
+        }),
+      },
+      getSecretMessage: {
+        useQuery: () => ({
+          data: "secret message",
+        }),
+      },
+    },
+  },
+}));
+
+describe("Sample test suite", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  it("should render the component", () => {
+    render(<Home />);
+    const main = within(screen.getByRole("main"));
+    expect(
+      main.getByRole("heading", { level: 1, name: "Create T3 App" })
+    ).toBeDefined();
+  });
+});
