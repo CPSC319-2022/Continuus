ad985d5f2fe8fe59397c0381505100af2023bf04
Scott King <scott.m.king@outlook.com>
2023-03-22 15:55:38 -0700

    Add schema and non-live UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,13 @@
 import { SearchIcon } from "~/icons/Search";
+import { ContributorRequestWidget } from "./contributor-request-widget/ContributorRequestWidget";
 import { ProfileMenu } from "./ProfileMenu";
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="h-full border-b border-b-gray-200 flex justify-between p-2">
+    <div className="flex h-full justify-between border-b border-b-gray-200 p-2">
       <p className="self-center text-xl font-bold">Continuus</p>
       <div className="flex items-center">
+        <ContributorRequestWidget className="mr-2" />
         <SearchIcon className="mr-2" />
         <ProfileMenu />
       </div>
---
+++ src/components/contributor-request-widget/ContributorRequestMenu.tsx
@@ -0,0 +1,89 @@
+import { type Dispatch, type SetStateAction, useEffect, useRef } from "react";
+import { api } from "~/utils/api";
+import { timeAgo } from "~/utils/time";
+import { ProfilePicture } from "../ProfilePicture";
+
+interface ContributorRequestMenuProps {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+export const ContributorRequestMenu: React.FC<ContributorRequestMenuProps> = ({
+  isOpen,
+  setIsOpen,
+}) => {
+  const { data: users } = api.user.paginatedUsers.useQuery({
+    pageIndex: 0,
+    pageSize: 10,
+  });
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  // https://stackoverflow.com/a/42234988
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (ref.current && !ref.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+    const handleEscapePreses = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        setIsOpen(false);
+      }
+    };
+    // Bind the event listener
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapePreses);
+    return () => {
+      // Unbind the event listener on clean up
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapePreses);
+    };
+  }, [ref, setIsOpen]);
+
+  return isOpen ? (
+    <div
+      ref={ref}
+      className="absolute right-5 top-10 max-h-[30rem] min-w-[18rem] rounded-md bg-white p-3 shadow-lg"
+    >
+      <div className="scrollbar-hide max-h-[28rem] overflow-scroll overflow-x-hidden">
+        <div className="sticky top-0 bg-white">
+          <p className="text-lg">Contributor Requests</p>
+          <hr />
+        </div>
+        {users && users.length ? (
+          users.map(({ id, name, createdAt, image }) => (
+            <div className="my-2 flex justify-between" key={id}>
+              <div className="flex">
+                <ProfilePicture
+                  className="self-center"
+                  imgUrl={image}
+                  size={2}
+                />
+                <div className="mx-2">
+                  <p>{name}</p>
+                  <p className="text-xs">{timeAgo(createdAt)}</p>
+                </div>
+              </div>
+              <div className="mr-1 flex self-center">
+                <p className="mx-1 rounded-md bg-highlight-green p-1 text-xs hover:cursor-pointer">
+                  Accept
+                </p>
+                <p className="mx-1 rounded-md bg-slate-200 p-1 text-xs hover:cursor-pointer">
+                  Reject
+                </p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div className="flex w-full justify-center">
+            <p className="my-5 text-sm italic text-slate-500">
+              No contributor requests
+            </p>
+          </div>
+        )}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+};
