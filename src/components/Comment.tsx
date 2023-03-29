f134dee2296cbb6cf6d58ba692cb30ae6646c783
Scott King <scott.m.king@outlook.com>
2023-03-28 19:33:57 -0700

    Merge dev

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -18,13 +18,13 @@ export interface CommentProps {
 }
 
 export const Comment: React.FC<CommentProps> = ({
-                                                  dateAdded,
-                                                  comment,
-                                                  name,
-                                                  userId,
-                                                  imgUrl,
-                                                  commentId,
-                                                }) => {
+  dateAdded,
+  comment,
+  name,
+  userId,
+  imgUrl,
+  commentId,
+}) => {
   const utils = api.useContext();
   const { status } = useSession();
   const [editInput, setEditInput] = useState<string>(comment);
@@ -34,7 +34,7 @@ export const Comment: React.FC<CommentProps> = ({
   const updateCommentMutation = api.comment.update.useMutation({
     onSuccess(data, variables, context) {
       return utils.comment.invalidate();
-    }
+    },
   });
 
   useEffect(() => {
@@ -45,12 +45,11 @@ export const Comment: React.FC<CommentProps> = ({
 
   const fetchedComment = api.comment.getUnique.useQuery({
     where: {
-      id: commentId
-    }
+      id: commentId,
+    },
   });
 
-  if (fetchedComment.data !== undefined)
-    comment = fetchedComment.data.content;
+  if (fetchedComment.data !== undefined) comment = fetchedComment.data.content;
 
   const shouldSeeEdit = () => {
     return isAuthed(status) && isAuthor(currUser.data, userId);
@@ -71,54 +70,75 @@ export const Comment: React.FC<CommentProps> = ({
   const handleCommentUpdate = () => {
     updateCommentMutation.mutate({
       where: {
-        id: commentId
+        id: commentId,
       },
       data: {
-        content: editInput
-      }
+        content: editInput,
+      },
     });
   };
 
   return (
     <>
-      <div className="bg-slate-100 p-3 m-3">
+      <div className="m-3 bg-slate-100 p-3">
         <div className="flex">
           <div className="avatar self-center">
-            <ProfilePicture size={2.5}
-                            redirectLink={userPathToProfile(userId)}
-                            imgUrl={imgUrl} />
+            <ProfilePicture
+              size={2.5}
+              redirectLink={userPathToProfile(userId)}
+              imgUrl={imgUrl}
+            />
           </div>
           <div className="ml-3">
             <ProfileName name={name || ""} userId={userId} />
             <p className="text-sm text-slate-400">{timeAgo(dateAdded)}</p>
           </div>
-          {
-            shouldSeeEdit() &&
-              <div className="ml-auto">
-                  <button className="p-1.5 bg-green-600 rounded-xl hover:rounded-3xl hover:bg-green-800 transition-all duration-300 text-white" onClick={() => {
-                    setIsEditing(true);
-                  }}>
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                           stroke="currentColor" stroke-width="2">
-                          <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                      </svg>
-                  </button>
-              </div>
-          }
+          {shouldSeeEdit() && (
+            <div className="ml-auto">
+              <button
+                className="rounded-xl bg-green-600 p-1.5 text-white transition-all duration-300 hover:rounded-3xl hover:bg-green-800"
+                onClick={() => {
+                  setIsEditing(true);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-6 w-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
+                  />
+                </svg>
+              </button>
+            </div>
+          )}
         </div>
-        {
-          updateCommentMutation.isLoading ?
-            <div className="flex h-96 w-full items-center justify-center">
-              <Spinner size={12} />
-            </div> : isEditing ?
-              <div>
-                <input type="text" value={editInput} onChange={handleInputChange}
-                       className="border-highlight-green bg-highlight-green" />
-                <button onClick={() => handleCommentUpdate()}>Save</button>
-                <button onClick={(event) => handleUpdateCancel(event)}>Cancel</button>
-              </div> : <p className="mt-4 w-full">{comment}</p>
-        }
+        {updateCommentMutation.isLoading ? (
+          <div className="flex h-96 w-full items-center justify-center">
+            <Spinner size={12} />
+          </div>
+        ) : isEditing ? (
+          <div>
+            <input
+              type="text"
+              value={editInput}
+              onChange={handleInputChange}
+              className="border-highlight-green bg-highlight-green"
+            />
+            <button onClick={() => handleCommentUpdate()}>Save</button>
+            <button onClick={(event) => handleUpdateCancel(event)}>
+              Cancel
+            </button>
+          </div>
+        ) : (
+          <p className="mt-4 w-full">{comment}</p>
+        )}
       </div>
     </>
   );
--- src/components/ProfilePicture.tsx
+++ src/components/ProfilePicture.tsx
@@ -1,10 +1,10 @@
 import Link from "next/link";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 interface ProfilePictureProps extends React.ComponentProps<"img"> {
   size: number;
   imgUrl?: string | null;
-  redirectLink?: string
+  redirectLink?: string;
 }
 /**
  * @prop size:          Width/Height of the Profile Picture in REM
@@ -14,28 +14,32 @@ interface ProfilePictureProps extends React.ComponentProps<"img"> {
  * @returns a profile picture rendered in a circle. Renders a default image if imgUrl isn't specified and has no redirect ink if redirectLink isn't specified
  */
 
-export const ProfilePicture: React.FC<ProfilePictureProps> = ({ size, imgUrl, redirectLink, ...props }) => {
-    const { className, ...restProps } = props;
-    const [imageUrl, setImageUrl] = useState<string>("https://i.stack.imgur.com/34AD2.jpg");
+export const ProfilePicture: React.FC<ProfilePictureProps> = ({
+  size,
+  imgUrl,
+  redirectLink,
+  ...props
+}) => {
+  const { className, ...restProps } = props;
+  const [imageUrl, setImageUrl] = useState<string>(
+    "https://i.stack.imgur.com/34AD2.jpg"
+  );
 
-    useEffect(() => {
-        if (imgUrl) {
-            setImageUrl(imgUrl);
-        }
-    }, [imgUrl]);
+  useEffect(() => {
+    if (imgUrl) {
+      setImageUrl(imgUrl);
+    }
+  }, [imgUrl]);
 
-    const img = <img 
-                    src={imageUrl} 
-                    alt="avatar"
-                    className={`inline-block aspect-square rounded-[50%] ${className ?? ""}`}
-                    style={{ width: `${size}rem` }}
-                    {...restProps}
-/>;
+  const img = (
+    <img
+      src={imageUrl}
+      alt="avatar"
+      className={`inline-block aspect-square rounded-[50%] ${className ?? ""}`}
+      style={{ width: `${size}rem` }}
+      {...restProps}
+    />
+  );
 
-    return (
-        <> {(redirectLink) ?
-            <Link href={redirectLink}>{img}</Link> :
-            img}
-        </>
-    )
-}
+  return <> {redirectLink ? <Link href={redirectLink}>{img}</Link> : img}</>;
+};
