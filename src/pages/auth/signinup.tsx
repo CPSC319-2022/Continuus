f45dbd8b4eba8f6ef82f66e6a4af305c82066647
Brendon <52805690+brendonson@users.noreply.github.com>
2023-04-03 10:03:25 -0700

    Css touchup (#98)

--- src/pages/auth/signinup.tsx
+++ src/pages/auth/signinup.tsx
@@ -7,7 +7,7 @@ import { SignInForm } from "~/components/SignInForm";
 import { SignUpForm } from "~/components/SignUpForm";
 
 
-const tabClassName = "flex-1 w-32 cursor-pointer border-b-4 border-solid border-white p-4 text-center transition-all hover:border-b-emerald-400 hover:font-bold";
+const tabClassName = "flex-1 w-32 cursor-pointer border-b-4 border-solid border-gray-200 p-4 text-center transition-all hover:border-b-emerald-400 hover:font-bold";
 const selectedTabClassName = "border-b-4 border-solid border-b-emerald-400 font-bold";
 
 const SignInUp: NextPage = () => {
@@ -22,7 +22,7 @@ const SignInUp: NextPage = () => {
 
   return (
     <main className="h-screen overflow-hidden flex items-center justify-center">
-      <div className="max-w-full min-w-0 border border-solid border-gray-200 rounded-md">
+      <div className="md:w-1/4 md:min-w-[40rem] w-5/6 border border-solid border shadow-md rounded-md">
         <Tabs>
           <TabList className="flex flex-row justify-start">
             <Tab className={tabClassName} selectedClassName={selectedTabClassName}>Sign In</Tab>
