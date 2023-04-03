24cf395e8036ab5d3126b667d96c9584dd971a2e
Brendon <52805690+brendonson@users.noreply.github.com>
2023-04-03 11:46:54 -0700

    Css touchup mobile fixes (#102)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ import { CommentModal } from "~/components/CommentModal";
 const Home: NextPage = () => {
   return (
     <Layout>
-      <div className="mt-4 flex min-h-screen w-full flex-col content-center items-center px-2 md:px-0 md:w-1/3 md:min-w-[40rem]">
+      <div className="mt-4 flex min-h-screen w-11/12 flex-col content-center items-center md:px-0 md:w-1/3 md:min-w-[40rem]">
         <BlogPostViewer/>
         <CreateBlogPostWidget />
         <CommentModal />
