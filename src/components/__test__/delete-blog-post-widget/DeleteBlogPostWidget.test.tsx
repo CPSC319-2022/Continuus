f102c5ef782809c03f0e1af2e02ec545b6f6f325
Scott King <scott.m.king@outlook.com>
2023-04-03 23:40:23 -0700

    Small additions

--- src/components/__test__/delete-blog-post-widget/DeleteBlogPostWidget.test.tsx
+++ src/components/__test__/delete-blog-post-widget/DeleteBlogPostWidget.test.tsx
@@ -1,7 +1,9 @@
 import { describe, it, expect, beforeEach, vi } from "vitest";
-import { cleanup, render, screen } from "@testing-library/react";
+import { cleanup, render, screen, waitFor } from "@testing-library/react";
 import { CommentProps } from "~/components/Comment";
 import userEvent from "@testing-library/user-event";
+
+const mutateFn = vi.fn();
 
 const setup = (isSuccess: boolean) => {
   vi.mock("~/utils/api", () => ({
@@ -11,6 +13,7 @@ const setup = (isSuccess: boolean) => {
           useMutation: () => ({
             isSuccess: false,
             isLoading: false,
+            mutate: mutateFn,
           }),
         },
       },
@@ -56,7 +59,7 @@ describe("Delete blog post widget: Snapshot", () => {
     expect(body.baseElement).toMatchSnapshot();
   });
 
-  it("delete successful", async () => {
+  it("delete confirmation", async () => {
     setup(true);
 
     const { DeleteBlogPostWidget } = await import(
@@ -65,6 +68,24 @@ describe("Delete blog post widget: Snapshot", () => {
 
     const body = render(<DeleteBlogPostWidget id="test-id" />);
     await userEvent.click(screen.getByTestId("delete-blog-post-button"));
+    await waitFor(() => {
+      expect(
+        screen.getByText("Are you sure you want to delete this post?")
+      ).toBeTruthy();
+    });
     expect(body.baseElement).toMatchSnapshot();
+  });
+
+  it("execute delete", async () => {
+    setup(true);
+
+    const { DeleteBlogPostWidget } = await import(
+      "~/components/delete-blog-post-widget/DeleteBlogPostWidget"
+    );
+
+    render(<DeleteBlogPostWidget id="test-id" />);
+    await userEvent.click(screen.getByTestId("delete-blog-post-button"));
+    await userEvent.click(screen.getByTestId("submit-delete"));
+    expect(mutateFn).toHaveBeenCalledTimes(1);
   });
 });
