fc984dbc9304e71f6603e4550fe29d256b4fe910
Altay Batuhan <altaybatuhanmail@gmail.com>
2023-04-03 21:45:12 -0700

    Using Next/Link instead of router.push

--- src/components/ProfileMenu.tsx
+++ src/components/ProfileMenu.tsx
@@ -2,12 +2,11 @@ import { Menu, MenuButton, MenuItem } from "@szhsin/react-menu";
 import { signOut } from "next-auth/react";
 import { userPathToProfile } from "~/utils/profile";
 import { api } from "~/utils/api";
-import { useRouter } from "next/router";
 import { CurrUserProfilePicture } from "./CurrUserProfilePicture";
+import Link from "next/link";
 
 export const ProfileMenu: React.FC = () => {
   const menuItemClassName = "w-full px-4 py-2 cursor-pointer text-center hover:bg-gray-200";
-  const router = useRouter();
   const currUser = api.user.currentUser.useQuery();
   const profile: string = userPathToProfile(currUser.data?.id || '');
 
@@ -23,12 +22,14 @@ export const ProfileMenu: React.FC = () => {
       offsetY={2}
       menuClassName="bg-white w-48 rounded-md border border-solid border-gray-400 py-2"
     >
-      <MenuItem
-        className={menuItemClassName}
-        key="profile"
-        onClick={() => void router.push(profile)}>
-        Profile
-      </MenuItem>
+      <Link href={profile}>
+        <MenuItem
+          className={menuItemClassName}
+          key="profile"
+        >
+          Profile
+        </MenuItem>
+      </Link>
       <MenuItem
         className={menuItemClassName}
         key="sign-out"
