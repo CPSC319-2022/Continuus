4241d44445cabcec5923d83cdb0511cea48d1b0c
itsarune <42703774+itsarune@users.noreply.github.com>
2023-04-12 00:00:27 -0700

    bucketing (#127)

--- terraform.tf
+++ terraform.tf
@@ -14,16 +14,3 @@ provider "google" {
   zone    = "northamerica-northeast2-a"
 }
 
-resource "random_id" "bucket_prefix" {
-  byte_length = 8
-}
-
-resource "google_storage_bucket" "default" {
-  name          = "${random_id.bucket_prefix.hex}-bucket-tfstate"
-  force_destroy = false
-  location      = "NORTHAMERICA-NORTHEAST2"
-  storage_class = "STANDARD"
-  versioning {
-    enabled = true
-  }
-}
